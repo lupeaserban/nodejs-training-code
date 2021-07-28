9ce1ad680b0dc94fc86ff6a877fd4e5627761f2a
Serban Lupea <lupea.serban@gmail.com>
2021-07-28 11:33:49 +0300

    Update and rename jsExercise.js to js-1.js

---
+++ js-1.js
@@ -0,0 +1,50 @@
+// Fix the code so that i declared in the if statement is a separate variable than i declared in the first line of the function. Be certain not to use the var keyword anywhere in your code.
+
+// function checkScope() {
+//   let i = 'function scope';
+//   if (true) {
+//     let i = 'block scope';
+//     console.log('Block scope i is: ', i); //block
+//   }
+//   console.log('Function scope i is: ', i); //function
+//   return i;
+// }
+
+// checkScope()
+
+// foo(); // "bar"
+
+// function foo() {
+//   console.log('bar');
+// }
+
+// let baz;
+
+// baz(); // TypeError: baz is not a function
+
+// baz = function() {
+//   console.log('bar2');
+// };
+
+  // let a  = 42
+  // let b = '30'
+
+  // console.log(a)
+
+  // console.log(b-a)
+
+  let w = new Boolean(false)
+  console.log(w.valueOf())
+
+  // console.log('4' == 4 ) //true
+  // console.log('4' === 4 ) //false
+
+  // '1.1' + '1.1'
+
+  // let x = (+'1.1') + (+'1.1')
+  // console.log(x)
+
+  let a = 'masina'
+  let b = 'moto'
+
+  console.log(`Acasa am o ${a} si o ${b}`) 
