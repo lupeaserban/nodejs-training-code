95b4b65de8dc4ada6a14c73260631dc5f4abf877
Serban Lupea <lupea.serban@gmail.com>
2021-08-05 12:23:42 +0300

    Update js-2.js

--- js-2.js
+++ js-2.js
@@ -1,35 +1,35 @@
 // for-of
 
-// for-in
+for-in
 
-// Error handling
+Error handling
 
-// function NamesStartingWithA(names) {
-//   const namesThatStartWithA = [];
+function NamesStartingWithA(names) {
+  const namesThatStartWithA = [];
   
-//     for (const name of names) {
-//       try {
-//         const nameWithA = readName(name);
-//         namesThatStartWithA.push(nameWithA);
-//       }
-//       catch (e) {
-//         console.log(e.message, name)
-//       }
-//   }
+    for (const name of names) {
+      try {
+        const nameWithA = readName(name);
+        namesThatStartWithA.push(nameWithA);
+      }
+      catch (e) {
+        console.log(e.message, name)
+      }
+  }
  
-//   return namesThatStartWithA
-// }
+  return namesThatStartWithA
+}
 
-// function readName(name) {
-//   if(!name.startsWith('A')) {
-//     const err = new Error();
-//     err.message = 'O crapat la';
-//     console.log(JSON.stringify(err))
-//     throw err
-//   }
-//   return name
-// }
+function readName(name) {
+  if(!name.startsWith('A')) {
+    const err = new Error();
+    err.message = 'O crapat la';
+    console.log(JSON.stringify(err))
+    throw err
+  }
+  return name
+}
 
 
-// const namesArray = [ 'Andre', 'Bogdan', 'Oana', 'Andreea' ] 
-// NamesStartingWithA(namesArray)
+const namesArray = [ 'Andre', 'Bogdan', 'Oana', 'Andreea' ] 
+NamesStartingWithA(namesArray)
