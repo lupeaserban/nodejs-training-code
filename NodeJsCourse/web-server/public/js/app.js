1d6999c3873311d5023526274f3f3ef190947190
MOA7CLJ <fixed-term.andrei.mostis@ro.bosch.com>
2021-08-13 23:04:04 +0300

    weather app done - ignored styling

--- NodeJsCourse/web-server/public/js/app.js
+++ NodeJsCourse/web-server/public/js/app.js
@@ -1,1 +1,29 @@
 console.log('Client side JS loaded!');
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.getElementById('messageOne');
+const messageTwo = document.getElementById('messageTwo');
+
+weatherForm.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    const location = search.value;
+
+    messageOne.textContent = 'Loading!';
+    messageTwo.textContent = '';
+
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                const { description, current, feelslike } = data.forecast;
+                messageTwo.textContent = `It is ${description} outside. There are ${current} degrees Celsius, but feels it like ${feelslike}.`;
+            }
+        });
+    });
+});
+
