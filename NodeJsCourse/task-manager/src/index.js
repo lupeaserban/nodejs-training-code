f7571d414b91c90aa9d3d84974a6b639ecb5ef06
MOA7CLJ <fixed-term.andrei.mostis@ro.bosch.com>
2021-08-18 16:21:03 +0300

    Update

--- NodeJsCourse/task-manager/src/index.js
+++ NodeJsCourse/task-manager/src/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
 
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
@@ -9,11 +7,41 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+//middleware
+
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled!');
+//     } else {
+//         next();
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('WebSite in maintenance!');
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+
+//Without middleware : new request -> run route handler
+//With middleware: new request -> do something -> run route handler
 
 app.listen(port, () => {
     console.log("Server is up on " + port);
 });
 
+const jwt = require('jsonwebtoken');
+
+const myFunction = async () => {
+    const token = jwt.sign({_id: 'abc123'}, 'thisismynewcourse', {expiresIn: '7 days'});
+    console.log(token);
+
+    console.log(jwt.verify(token, 'thisismynewcourse'));
+
+
+}
+
+myFunction();
--- NodeJsCourse/task-manager/src/routers/user.js
+++ NodeJsCourse/task-manager/src/routers/user.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const User = require('../models/user');
 const router = new express.Router();
+const auth = require('../middleware/auth');
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
 
     try {
         await user.save();
-        res.status(201).send(user);
+        const token = await user.generateAuthToken();
+        res.status(201).send({user, token});
     } catch (e) {
         res.status(400).send(e.message);
     }
@@ -19,8 +21,33 @@ router.post('/users', async (req, res) => {
     // });
 });
 
-router.get('/users', async (req, res) => {
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
+})
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        });
+        await req.user.save();
+        res.send();
+    } catch (e) {
+        res.status(500).send();
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req,res) => {
+    try{
+        req.user.tokens = [];
+        await req.user.save();
+        res.send();
+    } catch (e) {
+        res.status(500).send();
+    }
+})
+
+router.get('/users', auth, async (req, res) => {
     try {
         const users = await User.find({});
         res.send(users);
@@ -70,7 +97,13 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+
+        const user = await User.findById(req.params.id);
+
+        updates.forEach((update) => user[update] = req.body[update]);
+
+        await user.save();
 
         if (!user) {
             return res.status(404).send();
@@ -79,6 +112,16 @@ router.patch('/users/:id', async (req, res) => {
         res.send(user);
     } catch (e) {
         res.status(400).send(e.message);
+    }
+})
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        res.send({user, token});
+    } catch (e) {
+        res.status(400).send();
     }
 })
 
@@ -95,7 +138,6 @@ router.delete('/users/:id', async (req, res) => {
         res.status(500).send(e.message);
     }
 })
-
 
 
 module.exports = router;
