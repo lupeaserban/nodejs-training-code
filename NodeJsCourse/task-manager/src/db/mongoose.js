e13d5aa42e2cbd77db78ea81e1f590de977c864e
serban <serban.lupea@ro.bosch.com>
2021-08-26 13:57:22 +0300

    handle connection error

--- NodeJsCourse/task-manager/src/db/mongoose.js
+++ NodeJsCourse/task-manager/src/db/mongoose.js
@@ -4,6 +4,7 @@ mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
-});
+}).catch(error => console.log(`Caught: ${error}`));
+
 
 
