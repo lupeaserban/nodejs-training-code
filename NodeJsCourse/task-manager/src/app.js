791e5005575521b6f7bad4e1a1094654f03a8b8f
Andrei Mostis <43552031+Andmos98@users.noreply.github.com>
2021-08-30 11:54:54 +0300

    Added swagger

--- NodeJsCourse/task-manager/src/app.js
+++ NodeJsCourse/task-manager/src/app.js
@@ -3,9 +3,11 @@ require('./db/mongoose');
 
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
